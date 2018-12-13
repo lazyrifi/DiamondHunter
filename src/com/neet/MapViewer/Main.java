bc964149691ba126573bbbcce871e890683aa962
dominicnjw <45349090+dominicnjw@users.noreply.github.com>
2018-12-13 15:34:00 +0800

    Added Javadocs for Main class

--- src/com/neet/MapViewer/Main.java
+++ src/com/neet/MapViewer/Main.java
@@ -8,23 +8,36 @@ import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
+/**
+* The MapViewer program is a JavaFX application that
+* allows a user a view the full map in the game Diamond Hunter
+*/
 public class Main extends Application{
 	
 	private Stage mainStage;
 	private Scene mainScene;
 	
+	/**
+	 * This method initializes the GUI window of the application, sets
+	 * a title and calls the method to initialize the GUI elements 
+	 * and their controller
+	 */
 	@Override
 	public void start(Stage args) throws Exception {
 		mainStage = args;
-		mainStage.setTitle("MapViewer");
+		mainStage.setTitle("MapViewer");	// sets title
 		
 		initMainScene();
 		
 		mainStage.setScene(mainScene);
-		mainStage.show();
+		mainStage.show();					// show window on-screen
 	}
 	
 	
+	/**
+	 * This method loads the FXML file containing the GUI elements of the app
+	 * and sets it to the main scene that is displayed in the main stage
+	 */
 	private void initMainScene() {
 		try {
 			FXMLLoader loader = new FXMLLoader();
@@ -37,6 +50,10 @@ public class Main extends Application{
 	}
 	
 	
+	/**
+	 * Main method that launches the JavaFX application
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		launch(args);
 	}
